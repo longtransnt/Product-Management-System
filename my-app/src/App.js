1be17e04c4703fff821481a84e420cccabee39a0
Long Tran <45643952+longtransnt@users.noreply.github.com>
2021-11-28 11:38:20 +0700

    ADD Page Switch for Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,9 +9,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import logo from'./logo.svg';
 import { Nav } from 'react-bootstrap';
+import { useState } from 'react';
 import { NavLink } from 'react-bootstrap';
 
 function App() {
+  const [page, setPage] = useState('Product Table')
   return (
     <div className="App">
     <Navbar bg="dark" variant="dark">
@@ -27,28 +29,19 @@ function App() {
         React Bootstrap
         </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#ProductTable">Product Table</Nav.Link>
-          <Nav.Link href="#AddProduct">Add Product</Nav.Link>
-          <Nav.Link href="#UpdateProduct">Update Product</Nav.Link>
-          <Nav.Link href="#deleteProduct">Delete Product</Nav.Link>
+          <Nav.Link onClick={()=>setPage('Product Table')}>Product Table</Nav.Link>
+          <Nav.Link onClick={()=>setPage('Add Product')}>Add Product</Nav.Link>
+          <Nav.Link onClick={()=>setPage('Update Product')}>Update Product</Nav.Link>
+          <Nav.Link onClick={()=>setPage('Delete Product')}>Delete Product</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
 
-      <Sites>
-        <div label="Product Table">
-            <ProductTable/>
-        </div>
-        <div label="Add Product">
-            <AddProduct/>
-        </div>
-        <div label="Update Product">
-            <UpdateProduct/>
-        </div>
-        <div label="Delete Product">
-            <DeleteProduct/>
-        </div>
-      </Sites>
+      {page==='Product Table'? <ProductTable/>: ''} 
+      {page==='Add Product'? <AddProduct/>: ''}
+      {page==='Update Product'? <UpdateProduct/>: ''}
+      {page==='Delete Product'? <DeleteProduct/>: ''}
+
     </div>
   );
 }
